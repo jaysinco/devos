37c88c46ea044f93ff1ce3f4097d88c7371437a5
jaysinco <jaysinco@163.com>
2023-08-09 13:59:52 +0800

    remove config dir

--- build.sh
+++ build.sh
@@ -5,59 +5,51 @@ set -e
 # flags
 
 do_clean=0
-do_config=0
 do_build_image=0
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         -h)
             echo
-            echo "Usage: build.sh [targets]"
+            echo "Usage: build.sh [options] [targets]"
+            echo
+            echo "Build Options:"
+            echo "  -c           clean before build"
+            echo "  -h           print command line options"
             echo
             echo "Build Targets:" 
-            echo "  clean        clean up"
-            echo "  config       generate configure files"
             echo "  image        build image"
             echo
             exit 0
             ;;
-      clean) do_clean=1 && shift ;;
-     config) do_config=1 && shift ;;
+         -c) do_clean=1 && shift ;;
       image) do_build_image=1 && shift ;;
           *) echo "invalid argument: $1" && exit 1 ;;
     esac
 done
 
-function clean_func() {
-    echo "I: clean up"
+if [ $do_build_image -eq 0 -a $do_build_image -eq 0 ]; then
+    do_build_image=1
+fi
+
+function setup_func() {
+    git clone https://salsa.debian.org/live-team/live-build.git
+    cd live-build
+    dpkg-buildpackage -b -uc -us
+    dpkg -i ../live-build_20230502_all.deb
 }
 
-function config_func() {
-    echo "I: generate configure files"
-    lb config \
-        --architectures amd64 \
-        --distribution bookworm \
-        --archive-areas "main contrib non-free non-free-firmware" \
-        --mirror-bootstrap https://mirrors.163.com/debian \
-        --mirror-chroot https://mirrors.163.com/debian \
-        --mirror-chroot-security https://mirrors.163.com/debian-security \
-        --mirror-binary https://mirrors.163.com/debian \
-        --mirror-binary-security https://mirrors.163.com/debian-security \
-        --mirror-debian-installer https://mirrors.163.com/debian \
-        --apt-secure false
+function clean_func() {
+    sudo lb clean
 }
 
 function build_image_func() {
-    echo "I: build image"
+    lb config && \
     sudo lb build
 }
 
 if [ $do_clean -eq 1 ]; then
     clean_func
-fi \
-&& \
-if [ $do_config -eq 1 ]; then
-    config_func
 fi \
 && \
 if [ $do_build_image -eq 1 ]; then
