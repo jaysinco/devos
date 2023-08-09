f3528d33b40009c096013c06c97d3656493245f9
jaysinco <jaysinco@163.com>
2023-08-09 20:53:35 +0800

    remove clean flag

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -e
 
 # flags
 
-do_clean=0
 do_build_image=0
 
 while [[ $# -gt 0 ]]; do
@@ -14,22 +13,19 @@ while [[ $# -gt 0 ]]; do
             echo "Usage: build.sh [options] [targets]"
             echo
             echo "Build Options:"
-            echo "  -c           clean before build"
             echo "  -h           print command line options"
             echo
-            echo "Build Targets:" 
+            echo "Build Targets:"
             echo "  image        build image"
             echo
             exit 0
             ;;
-         -c) do_clean=1 && shift ;;
       image) do_build_image=1 && shift ;;
           *) echo "invalid argument: $1" && exit 1 ;;
     esac
 done
 
 if [ $do_build_image -eq 0 -a $do_build_image -eq 0 ]; then
-    do_clean=1
     do_build_image=1
 fi
 
@@ -42,21 +38,15 @@ function setup_func() {
     dpkg -i ../live-build_20230502_all.deb
 }
 
-function clean_func() {
-    sudo lb clean
-}
-
 function build_image_func() {
+    sudo lb clean \
+    && \
     lb config \
     && \
     sudo lb build
 }
 
 pushd $script_dir \
-&& \
-if [ $do_clean -eq 1 ]; then
-    clean_func
-fi \
 && \
 if [ $do_build_image -eq 1 ]; then
     build_image_func
